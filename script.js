043394c8031e15ff0584be260372e254aba35eaa
Ben Chan <ben@chanhq.com>
2020-12-09 15:27:06 +0000

    Added a real world example of promise

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ promise
     console.log(result2);
   })
   .catch(() => console.log("Error"));
+
+// Promise real world example
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums",
+];
+
+Promise.all(
+  urls.map((url) => {
+    return fetch(url).then((resp) => resp.json());
+  })
+)
+  .then((results) => {
+    console.log(results[0]);
+    console.log(results[1]);
+    console.log(results[2]);
+  })
+  .catch(() => console.log("error"));
