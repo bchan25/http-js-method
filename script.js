9ae766687225eaef5cab07b6a5f4827081e5b552
Ben Chan <ben@chanhq.com>
2020-12-09 15:23:08 +0000

    Simple Promise Example

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+// Callbacks - Once something done do this
+
+// Promise
+const promise = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("Stuff Worked");
+  } else {
+    reject("Error, it broke");
+  }
+});
+
+// calling promise
+//promise.then((result) => console.log(result));
+
+promise
+  .then((result) => result + "!")
+  .then((result2) => {
+    console.log(result2);
+  })
+  .catch(() => console.log("Error"));
